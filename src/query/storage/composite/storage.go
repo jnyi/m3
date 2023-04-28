a5f5256284d0e980bdba3d3b2a74a0539616d957
Yi Jin <yi.jin@databricks.com>
2023-04-27 19:26:06 -0700

    enable read
    Signed-off-by: Yi Jin <yi.jin@databricks.com>

--- src/query/storage/composite/storage.go
+++ src/query/storage/composite/storage.go
@@ -15,41 +15,30 @@ import (
 
 type compositeStorage struct {
 	name   string
-	m3     storage.Storage
 	stores []storage.Storage
 	logger *zap.Logger
 }
 
-func Compose(logger *zap.Logger, m3 storage.Storage, stores ...storage.Storage) storage.Storage {
+func Compose(logger *zap.Logger, stores ...storage.Storage) storage.Storage {
 	var name strings.Builder
-	name.WriteString(m3.Name())
-	name.WriteByte('-')
-	for _, s := range stores {
-		name.WriteString(s.Name())
-		name.WriteByte('-')
-	}
 	name.WriteString("compositedStorage")
 	logger.Info("construct a composite storage",
 		zap.String("name", name.String()),
 		zap.Int("store_size", len(stores)+1))
 	return &compositeStorage{
 		name:   name.String(),
-		m3:     m3,
 		stores: stores,
 		logger: logger,
 	}
 }
 
 func (s *compositeStorage) Write(ctx context.Context, query *storage.WriteQuery) error {
-	if err := s.m3.Write(ctx, query); err != nil {
-		// we only report error for m3 storage
-		return err
-	}
 	for _, store := range s.stores {
 		if err := store.Write(ctx, query); err != nil {
 			s.logger.Error("composite storage write error",
 				zap.String("store", store.Name()),
 				zap.Error(err))
+			return err
 		}
 	}
 	return nil
@@ -68,65 +57,98 @@ func (s *compositeStorage) Name() string {
 }
 
 func (s *compositeStorage) Close() error {
-	if err := s.m3.Close(); err != nil {
-		// we only report error for m3 storage
-		return err
-	}
 	for _, store := range s.stores {
 		if err := store.Close(); err != nil {
 			s.logger.Error("composite storage close error",
 				zap.String("store", store.Name()),
 				zap.Error(err))
+			return err
 		}
 	}
 	return nil
 }
 
 func (s *compositeStorage) FetchProm(
-	_ context.Context,
-	_ *storage.FetchQuery,
-	_ *storage.FetchOptions,
+	c context.Context,
+	q *storage.FetchQuery,
+	opts *storage.FetchOptions,
 ) (storage.PromResult, error) {
+	for _, store := range s.stores {
+		r, err := store.FetchProm(c, q, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return storage.PromResult{}, unimplementedError(s.name, "FetchProm")
 }
 
 func (s *compositeStorage) FetchBlocks(
-	_ context.Context,
-	_ *storage.FetchQuery,
-	_ *storage.FetchOptions,
+	c context.Context,
+	q *storage.FetchQuery,
+	opts *storage.FetchOptions,
 ) (block.Result, error) {
+	for _, store := range s.stores {
+		r, err := store.FetchBlocks(c, q, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return block.Result{}, unimplementedError(s.name, "FetchBlocks")
 }
 
 func (s *compositeStorage) FetchCompressed(
-	_ context.Context,
-	_ *storage.FetchQuery,
-	_ *storage.FetchOptions,
+	c context.Context,
+	q *storage.FetchQuery,
+	opts *storage.FetchOptions,
 ) (consolidators.MultiFetchResult, error) {
+	for _, store := range s.stores {
+		r, err := store.FetchCompressed(c, q, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return nil, unimplementedError(s.name, "FetchCompressed")
 }
 
 func (s *compositeStorage) SearchSeries(
-	_ context.Context,
-	_ *storage.FetchQuery,
-	_ *storage.FetchOptions,
+	c context.Context,
+	q *storage.FetchQuery,
+	opts *storage.FetchOptions,
 ) (*storage.SearchResults, error) {
+	for _, store := range s.stores {
+		r, err := store.SearchSeries(c, q, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return nil, unimplementedError(s.name, "SearchSeries")
 }
 
 func (s *compositeStorage) CompleteTags(
-	_ context.Context,
-	_ *storage.CompleteTagsQuery,
-	_ *storage.FetchOptions,
+	c context.Context,
+	q *storage.CompleteTagsQuery,
+	opts *storage.FetchOptions,
 ) (*consolidators.CompleteTagsResult, error) {
+	for _, store := range s.stores {
+		r, err := store.CompleteTags(c, q, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return nil, unimplementedError(s.name, "CompleteTags")
 }
 
 func (s *compositeStorage) QueryStorageMetadataAttributes(
-	_ context.Context,
-	_, _ time.Time,
-	_ *storage.FetchOptions,
+	c context.Context,
+	start, end time.Time,
+	opts *storage.FetchOptions,
 ) ([]storagemetadata.Attributes, error) {
+	for _, store := range s.stores {
+		r, err := store.QueryStorageMetadataAttributes(c, start, end, opts)
+		if err == nil {
+			return r, err
+		}
+	}
 	return nil, unimplementedError(s.name, "QueryStorageMetadataAttributes")
 }
 
