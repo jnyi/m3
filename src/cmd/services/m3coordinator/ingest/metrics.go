b05c1a284a9fa79a2345afe2f02c96a0af7ca52d
Christopher Li <christopher.li@databricks.com>
2024-08-19 21:09:37 -0700

    reduce write latency buckets

--- src/cmd/services/m3coordinator/ingest/metrics.go
+++ src/cmd/services/m3coordinator/ingest/metrics.go
@@ -37,51 +37,51 @@ type LatencyBuckets struct {
 // ingested) and write latency (i.e. time from received a sample from remote
 // source to completion of that write locally).
 func NewLatencyBuckets() (LatencyBuckets, error) {
-	upTo1sBuckets, err := tally.LinearDurationBuckets(0, 100*time.Millisecond, 10)
+	upTo1sBuckets, err := tally.LinearDurationBuckets(0, 200*time.Millisecond, 5)
 	if err != nil {
 		return LatencyBuckets{}, err
 	}
 
-	upTo10sBuckets, err := tally.LinearDurationBuckets(time.Second, 500*time.Millisecond, 18)
+	upTo10sBuckets, err := tally.LinearDurationBuckets(time.Second, 3000*time.Millisecond, 3)
 	if err != nil {
 		return LatencyBuckets{}, err
 	}
 
-	upTo60sBuckets, err := tally.LinearDurationBuckets(10*time.Second, 5*time.Second, 11)
+	upTo60sBuckets, err := tally.LinearDurationBuckets(10*time.Second, 25*time.Second, 3)
 	if err != nil {
 		return LatencyBuckets{}, err
 	}
 
-	upTo60mBuckets, err := tally.LinearDurationBuckets(0, 5*time.Minute, 12)
+	upTo15mBuckets, err := tally.LinearDurationBuckets(0, 5*time.Minute, 4)
 	if err != nil {
 		return LatencyBuckets{}, err
 	}
-	upTo60mBuckets = upTo60mBuckets[1:] // Remove the first 0s to get 5 min aligned buckets
+	upTo15mBuckets = upTo15mBuckets[1:] // Remove the first 0s to get 5 min aligned buckets
 
-	upTo6hBuckets, err := tally.LinearDurationBuckets(time.Hour, 30*time.Minute, 12)
+	upTo2hBuckets, err := tally.LinearDurationBuckets(time.Hour, 30*time.Minute, 4)
 	if err != nil {
 		return LatencyBuckets{}, err
 	}
 
-	upTo24hBuckets, err := tally.LinearDurationBuckets(6*time.Hour, time.Hour, 19)
-	if err != nil {
-		return LatencyBuckets{}, err
-	}
-	upTo24hBuckets = upTo24hBuckets[1:] // Remove the first 6h to get 1 hour aligned buckets
+	//upTo24hBuckets, err := tally.LinearDurationBuckets(6*time.Hour, time.Hour, 19)
+	//if err != nil {
+	//	return LatencyBuckets{}, err
+	//}
+	//upTo24hBuckets = upTo24hBuckets[1:] // Remove the first 6h to get 1 hour aligned buckets
 
 	var writeLatencyBuckets tally.DurationBuckets
 	writeLatencyBuckets = append(writeLatencyBuckets, upTo1sBuckets...)
 	writeLatencyBuckets = append(writeLatencyBuckets, upTo10sBuckets...)
 	writeLatencyBuckets = append(writeLatencyBuckets, upTo60sBuckets...)
-	writeLatencyBuckets = append(writeLatencyBuckets, upTo60mBuckets...)
+	writeLatencyBuckets = append(writeLatencyBuckets, upTo15mBuckets...)
 
 	var ingestLatencyBuckets tally.DurationBuckets
 	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo1sBuckets...)
 	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo10sBuckets...)
 	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo60sBuckets...)
-	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo60mBuckets...)
-	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo6hBuckets...)
-	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo24hBuckets...)
+	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo15mBuckets...)
+	ingestLatencyBuckets = append(ingestLatencyBuckets, upTo2hBuckets...)
+	//ingestLatencyBuckets = append(ingestLatencyBuckets, upTo24hBuckets...)
 
 	return LatencyBuckets{
 		WriteLatencyBuckets:  writeLatencyBuckets,
